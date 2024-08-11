9dba2fe013e5276ecd4522ce73611ca62c734a8d
TiagoLuz9292 <tiagoluz92@gmail.com>
2024-08-11 18:20:48 +0100

    updates

--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,13 +1,13 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
-import { FaUserCircle, FaSearch } from 'react-icons/fa'; // Import FaSearch
+import { FaUserCircle, FaSearch } from 'react-icons/fa';
 import { toast } from 'react-toastify';
 import { Dropdown, DropdownToggle, DropdownMenu, DropdownItem } from 'reactstrap';
 import AuthModal from './Auth/AuthModal';
 
 const Navbar = () => {
     const navigate = useNavigate();
-    const isLoggedIn = !!localStorage.getItem('token');
+    const [isLoggedIn, setIsLoggedIn] = useState(!!localStorage.getItem('token'));
     const [dropdownOpen, setDropdownOpen] = useState(false);
     const [modalOpen, setModalOpen] = useState(false);
 
@@ -17,29 +17,24 @@ const Navbar = () => {
     const handleLogout = () => {
         localStorage.removeItem('token');
         localStorage.removeItem('userId');
-        toast.success('Logout successful', {
-            position: "top-right",
-            autoClose: 3000,
-        });
-        navigate('/login');
-        window.location.reload();
+        setIsLoggedIn(false);
+        toast.success('Logout successful');
+        navigate('/');
     };
 
     return (
         <nav className="navbar navbar-expand-lg" style={{ backgroundColor: '#333333', color: '#ffffff' }}>
             <div className="container-fluid">
-            <Link className="navbar-brand" to="/" style={{ color: '#ffffff' }}>Online Marketplace</Link>
+                <Link className="navbar-brand" to="/" style={{ color: '#ffffff' }}>Online Marketplace</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarNav">
                     <ul className="navbar-nav">
                         {isLoggedIn && (
-                            <>
-                                <li className="nav-item">
-                                    <Link className="nav-link" to="/add-product" style={{ color: '#ffffff', fontSize: '18px' }}>Add Product</Link>
-                                </li>
-                            </>
+                            <li className="nav-item">
+                                <Link className="nav-link" to="/add-product" style={{ color: '#ffffff', fontSize: '18px' }}>Add Product</Link>
+                            </li>
                         )}
                     </ul>
                     <form className="d-flex search-bar-container">
@@ -71,8 +66,8 @@ const Navbar = () => {
                             </Dropdown>
                         ) : (
                             <>
-                                 <button className="btn btn-outline-primary me-2" onClick={toggleModal}>Register</button>
-                                 <button className="btn btn-outline-primary" onClick={toggleModal}>Login</button>
+                                <button className="btn btn-outline-primary me-2" onClick={toggleModal}>Register</button>
+                                <button className="btn btn-outline-primary" onClick={toggleModal}>Login</button>
                             </>
                         )}
                     </div>
